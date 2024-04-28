cf789733250798f2990144303dded9b027dbf606
MufanColin <76479709+MufanColin@users.noreply.github.com>
2024-04-28 21:26:34 +0800

    finished ant project optional problem 1, passed all tests

--- ants-modified/ants.py
+++ ants-modified/ants.py
@@ -106,6 +106,7 @@ class Ant(Insect):
 
     # ADD CLASS ATTRIBUTES HERE
     has_been_doubled = False
+    blocks_path = True
     def __init__(self, health=1):
         super().__init__(health)
 
@@ -484,14 +485,19 @@ class NinjaAnt(Ant):
     damage = 1
     food_cost = 5
     # OVERRIDE CLASS ATTRIBUTES HERE
+    blocks_path = False  # NinjaAnt can never be stung because it does not block path.
+
     # BEGIN Problem Optional 1
-    implemented = False  # Change to True to view in the GUI
+    implemented = True  # Change to True to view in the GUI
 
     # END Problem Optional 1
 
     def action(self, gamestate):
         # BEGIN Problem Optional 1
         "*** YOUR CODE HERE ***"
+        bees_copy = self.place.bees[:]
+        for bee in bees_copy:
+            bee.reduce_health(self.damage)
         # END Problem Optional 1
 
 
@@ -558,7 +564,7 @@ class Bee(Insect):
         """Return True if this Bee cannot advance to the next Place."""
         # Special handling for NinjaAnt
         # BEGIN Problem Optional 1
-        return self.place.ant is not None
+        return self.place.ant is not None and self.place.ant.blocks_path
         # END Problem Optional 1
 
     def action(self, gamestate):
